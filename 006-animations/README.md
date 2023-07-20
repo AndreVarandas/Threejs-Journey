a1d6042fa81a701277960dc49bb9f3091fa9379a
André Varandas <andre.m.varandas@gmail.com>
2023-07-20 16:57:04 +0100

    finish 006 animations

--- 006-animations/README.md
+++ 006-animations/README.md
@@ -4,10 +4,14 @@ Animations are added by changing the position or rotation of a mesh over time.
 
 Delta time is the time between frames. It is used to make sure that the animation is smooth and not dependent on the frame rate. It is calculated by subtracting the current time from the previous time.
 
+requestAnimationFrame is a function that runs a function on every frame. It can be used to run the animation loop.
+
 **Key points**
 
 -   Animations are added by changing the position or rotation of a mesh over time.
 -   Delta time is the time between frames.
+-   We can use the `requestAnimationFrame` function to run a function on every frame.
+-   External libraries like `gsap` can be used to make animations easier.
 
 ## Run it
 
--- 006-animations/script.js
+++ 006-animations/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 const canvas = document.querySelector('canvas.webgl')
 
@@ -29,21 +30,21 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
 // Time
-let time = Date.now()
+const clock = new THREE.Clock()
 
 // Animations
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+
 const tick = () => {
   // Time
-  const currentTime = Date.now()
-  const deltaTime = currentTime - time
-  time = currentTime
-
-  // Update objects
-  mesh.rotation.x += 0.001 * deltaTime
-
-  // Render
+  // const elapsedTime = clock.getElapsedTime()
+  // // Update objects
+  // mesh.position.y = Math.sin(elapsedTime)
+  // mesh.position.x = Math.cos(elapsedTime)
+  // // Render
   renderer.render(scene, camera)
-
   window.requestAnimationFrame(tick)
 }
 
