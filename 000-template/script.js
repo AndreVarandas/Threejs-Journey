6e67455d8ca4f907caabb461ac785b22c861f411
André Varandas <andre.m.varandas@gmail.com>
2023-07-23 17:25:12 +0100

    update template repo

--- 000-template/script.js
+++ 000-template/script.js
@@ -1,3 +1,75 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-console.log('Three.js version:', THREE.REVISION)
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+// Resize window on resize
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Fullscreen
+window.addEventListener('dblclick', () =>
+  document.fullscreenElement
+    ? document.exitFullscreen()
+    : canvas.requestFullscreen(),
+)
+
+// Red cube
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+)
+scene.add(cube)
+
+// Create camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+)
+camera.position.z = 3
+camera.lookAt(cube.position)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Animations
+
+const tick = () => {
+  // Update objects
+  controls.update()
+
+  // // Render
+  renderer.render(scene, camera)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+  window.requestAnimationFrame(tick)
+}
+
+tick()
